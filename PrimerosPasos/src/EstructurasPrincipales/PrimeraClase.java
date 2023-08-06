91d4538fed91389f7ba2817fd0247d82600410c3
ManuelMkd <manuel.makde@gmail.com>
2023-08-06 13:35:45 +0200

    Segunda clase Variable

--- PrimerosPasos/src/EstructurasPrincipales/PrimeraClase.java
+++ PrimerosPasos/src/EstructurasPrincipales/PrimeraClase.java
@@ -1,9 +1,8 @@
 package EstructurasPrincipales;
 
-public class Estructuras_1 {
+public class PrimeraClase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		System.out.println("Hola mundo");
 	}
---
+++ PrimerosPasos/src/EstructurasPrincipales/Variables.java
@@ -0,0 +1,16 @@
+package EstructurasPrincipales;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		int edad = 35;
+		
+		System.out.println(edad);
+		
+		edad = 75;
+		
+		System.out.println(edad);
+	}
+
+}
