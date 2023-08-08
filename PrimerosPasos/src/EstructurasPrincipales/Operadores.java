04f867115b8651c8d8a541e23f37b289536f0fe6
ManuelMkd <manuel.makde@gmail.com>
2023-08-08 09:20:12 +0200

    Operadores

---
+++ PrimerosPasos/src/EstructurasPrincipales/Operadores.java
@@ -0,0 +1,19 @@
+package EstructurasPrincipales;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+
+		double a = 10;
+		double b;
+		b = 2;
+		double c = a/b;
+		
+		//c-=4;
+		
+		
+		System.out.println(c);
+		
+	}
+
+}
---
+++ PrimerosPasos/src/EstructurasPrincipales/Operadores_2.java
@@ -0,0 +1,25 @@
+package EstructurasPrincipales;
+
+public class Operadores_2 {
+
+	public static void main(String[] args) {
+
+//		final double a_pulgadas = 2.54;
+//		
+//		double cm = 6;
+//		
+//		double resultado = cm / a_pulgadas;
+//		
+//		System.out.println("En " + cm + "cm hay " + resultado + " pulgadas.");
+		
+		int op1, op2, result;
+		
+		op1 = 9;
+		op2 = 7;
+		result = op1 + op2;
+		
+		System.out.println(result);
+		
+	}
+
+}
